ddba0899c6d30f538735ea02cc860389e5f0b7c2
SAAS171 <53257963+SAAS171@users.noreply.github.com>
2021-10-02 21:58:25 +0500

    FYP-2-3evaluation

---
+++ Ali Asghar/User-Forum-Noti-search/client/src/components/global/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="text-center bg-light py-4">
+      <h6></h6>
+     
+      <p></p>
+    </div>
+  )
+}
+
+export default Footer
--- Compiler, App Initialization, Code Blocks/server/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    clientId: "a61805994b725119b2620c9c30a43988",
-    clientSecret:"4edd3a8086acff1ddc1aa402dd5ae3cf931b99daaaa3be719f8fafcbca1f6548"
-}
